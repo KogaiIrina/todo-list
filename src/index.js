4a32dfb9301bafd75888b57cc587f8b62be021b8
Kogay Irina <kogayie1995@gmail.com>
2021-01-08 15:37:56 +0300

    Created todo list.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {todos: []};
+        this.inputRef = React.createRef();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/todos')
+            .then(response => response.json())
+            .then(todos => this.setState({todos}));
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        fetch('http://localhost:3001/todo', {
+            method: 'POST',
+            body: this.inputRef.current.value,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+            .then(response => response.json())
+            .then(todo => {
+                this.setState({todos: [...this.state.todos, todo]});
+                this.inputRef.current.value = '';
+            });
+    }
+  
+    render() {
+        const renderedTodos = this.state.todos.map((todo, i) => (
+            <div key={i}>{todo.summary}</div>
+        ));
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Todo:
+                        <input type="text" ref={this.inputRef} required/>
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+                <div>{renderedTodos}</div>
+            </div>
+        );
+    }
+
+  }
+
+
+
+  ReactDOM.render(
+    <TodoForm />,
+    document.getElementById('root')
+  );
