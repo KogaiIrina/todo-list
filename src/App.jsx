11e6f0d5c39646cb2099814c01ce4da12d684b1d
Kogay Irina <kogayie1995@gmail.com>
2021-01-29 18:05:30 +0300

    added new form for rendering Todo and Login forms

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import TodoForm from './TodoForm';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false
+        };
+    }
+
+    render() {
+        if (!this.state.isLoggedIn) {
+            return (<LoginForm />);
+        } else {
+            return (<TodoForm />);
+        }
+    }
+}
