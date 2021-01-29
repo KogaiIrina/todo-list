3a93479282796e697343471186d4c43d6fd6aea5
Kogay Irina <kogayie1995@gmail.com>
2021-01-29 18:01:57 +0300

    Added new file App.jsx for rendering Todo and Login forms.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LoginForm from './LoginForm';
+import App from './App';
 
 
 ReactDOM.render(
-    <LoginForm />,
+    <App />,
     document.getElementById('root')
 );
