d2f961df66e97912ea3d11ac932b5d3fcff957af
Kogay Irina <kogayie1995@gmail.com>
2021-01-30 20:30:43 +0300

    Added new function to rendering LogIn form.

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './LoginForm.css';
 
 export default class LoginForm extends React.Component {
-   
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleSubmit() {
+        this.props.onLogged();
+    }
+
     render() {
         return (
-            <form className="LoginForm">
+            <form className="LoginForm" onSubmit={this.handleSubmit}>
                 <h2>Log In</h2>
                 <div>
                     <label>email
