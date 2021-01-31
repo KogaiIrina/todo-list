d0fa4cbfcfdbf7901865e2caa3559900547ae470
Kogay Irina <kogayie1995@gmail.com>
2021-01-31 17:05:47 +0300

    Added user schema

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// const ObjectId = require('mongodb').ObjectId;
 
 mongoose.connect('mongodb://localhost:27017/todo-list', { useNewUrlParser: true });
 
@@ -7,12 +8,36 @@ const todoSchema = new mongoose.Schema({
   summary: {
     type: String,
     required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
   timestamps: true
 });
 
+todoSchema.index({ user: 1 });
 
 const Todo = mongoose.model('Todo', todoSchema);
 
-module.exports = Todo;
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+userSchema.index({ email: 1 }, { unique: true });
+
+const User = mongoose.model('User', userSchema);
+
+exports.Todo = Todo;
+exports.User = User;
