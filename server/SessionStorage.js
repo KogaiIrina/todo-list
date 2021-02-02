e194496d2fa513b46ec3472417e018947c181e62
Kogay Irina <kogayie1995@gmail.com>
2021-02-02 22:24:27 +0300

    WIP: Added function to deleting session

--- server/SessionStorage.js
+++ server/SessionStorage.js
@@ -1,5 +1,27 @@
 import crypto from 'crypto';
 
+// export default class SessionStorage {
+//     constructor() {
+//         this.storage = {};
+//     }
+
+//     createSession(data) {
+//         let key;
+//         do {
+//             key = crypto.randomBytes(256).toString('hex');
+//         } while (key in this.storage);
+
+//         this.storage[key] = data;
+
+//         return key;
+//     }
+
+//     getSession(key) {
+//         return this.storage[key];
+//     }
+// }
+
+
 export default class SessionStorage {
     constructor() {
         this.storage = {};
@@ -7,16 +29,27 @@ export default class SessionStorage {
 
     createSession(data) {
         let key;
+        let date = +new Date();
         do {
             key = crypto.randomBytes(256).toString('hex');
         } while (key in this.storage);
 
-        this.storage[key] = data;
-
+        this.storage[key] = { data, date };
         return key;
     }
 
     getSession(key) {
-        return this.storage[key];
+        return this.storage[key].data;
+    }
+
+    deleteSession() {
+        let newDate = +new Date();
+        setInterval(() => {
+            let difInTime = newDate - (1000 * 60 *1440 * 7);
+            if (this.storage[key].date >= difInTime) {
+                delete this.storage[key];
+            }
+            return this.storage[key].date;
+        }, 43200000);
     }
 }
--- server/tests/SessionStorage.test.js
+++ server/tests/SessionStorage.test.js
@@ -4,27 +4,38 @@ describe('SessionStorage', () => {
     const sessionStorage = new SessionStorage();
     let key;
 
-    test('should return undefined on non-existent key', () => {
-        expect(sessionStorage.getSession('non-existent key')).toBeUndefined();
-    });
+    // test('should return undefined on non-existent key', () => {
+    //     expect(sessionStorage.getSession('non-existent key')).toBeUndefined();
+    // });
 
-    test('should create session and store data', () => {
+    // test('should create session and store data', () => {
+    //     key = sessionStorage.createSession('important data');
+    //     expect(typeof key).toBe('string');
+    //     expect(sessionStorage.getSession(key)).toBe('important data');
+    // });
+
+    // test('should return undefined on key from another session storage', () => {
+    //     const anotherSessionStorage = new SessionStorage();
+    //     expect(anotherSessionStorage.getSession(key)).toBeUndefined();
+    // });
+
+    // test('should allow creation of more than one session', () => {
+    //     const oneMoreKey = sessionStorage.createSession('another data');
+    //     expect(typeof oneMoreKey).toBe('string');
+    //     expect(oneMoreKey).not.toBe(key);
+    //     expect(sessionStorage.getSession(oneMoreKey)).toBe('another data');
+    //     expect(sessionStorage.getSession(key)).toBe('important data');
+    // });
+    jest.useFakeTimers();
+    test('waits 7 days before deleting session', () => {
         key = sessionStorage.createSession('important data');
         expect(typeof key).toBe('string');
         expect(sessionStorage.getSession(key)).toBe('important data');
-    });
+        expect(sessionStorage.deleteSession).toHaveBeenCalledTimes(1);
+        expect(sessionStorage.deleteSession).toHaveBeenLastCalledWith(expect.any(Function), 1612897034346
+        );
+        expect(sessionStorage.deleteSession).toBeUndefined();
 
-    test('should return undefined on key from another session storage', () => {
-        const anotherSessionStorage = new SessionStorage();
-        expect(anotherSessionStorage.getSession(key)).toBeUndefined();
-    });
-
-    test('should allow creation of more than one session', () => {
-        const oneMoreKey = sessionStorage.createSession('another data');
-        expect(typeof oneMoreKey).toBe('string');
-        expect(oneMoreKey).not.toBe(key);
-        expect(sessionStorage.getSession(oneMoreKey)).toBe('another data');
-        expect(sessionStorage.getSession(key)).toBe('important data');
     });
 });
 
