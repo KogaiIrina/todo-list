6f08b9d233d0a93e8def2cd8d6c0cc4b26045524
Kogay Irina <kogayie1995@gmail.com>
2021-02-02 14:38:20 +0300

    Added jest

---
+++ server/tests/SessionStorage.test.js
@@ -0,0 +1,30 @@
+import SessionStorage from '../SessionStorage';
+
+describe('SessionStorage', () => {
+    const sessionStorage = new SessionStorage();
+    let key;
+
+    test('should return undefined on non-existent key', () => {
+        expect(sessionStorage.getSession('non-existent key')).toBeUndefined();
+    });
+
+    test('should create session and store data', () => {
+        key = sessionStorage.createSession('important data');
+        expect(typeof key).toBe('string');
+        expect(sessionStorage.getSession(key)).toBe('important data');
+    });
+
+    test('should return undefined on key from another session storage', () => {
+        const anotherSessionStorage = new SessionStorage();
+        expect(anotherSessionStorage.getSession(key)).toBeUndefined();
+    });
+
+    test('should allow creation of more than one session', () => {
+        const oneMoreKey = sessionStorage.createSession('another data');
+        expect(typeof oneMoreKey).toBe('string');
+        expect(oneMoreKey).not.toBe(key);
+        expect(sessionStorage.getSession(oneMoreKey)).toBe('another data');
+        expect(sessionStorage.getSession(key)).toBe('important data');
+    });
+});
+
