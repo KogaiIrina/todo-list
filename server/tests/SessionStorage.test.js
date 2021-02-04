ee3be5dd4872e8798947d00fde3c0150bb3abc45
Kogay Irina <kogayie1995@gmail.com>
2021-02-04 23:29:36 +0300

    Finished SessionStorage

--- server/tests/SessionStorage.test.js
+++ server/tests/SessionStorage.test.js
@@ -7,7 +7,9 @@ describe('SessionStorage', () => {
     let key;
 
     afterEach(() => {
-        jest.resetAllMocks();
+        jest.restoreAllMocks();
+        jest.clearAllTimers();
+        sessionStorage.stopExpirationInterval();
     });
 
     test('should return undefined on non-existent key', () => {
@@ -41,6 +43,66 @@ describe('SessionStorage', () => {
         jest.spyOn(Date, 'now').mockReturnValue(111222333);
         sessionStorage.getSession(key);
         expect(sessionStorage._storage[key].accessedAt).toBe(111222333);
+    });
+
+    test('.deleteExpired should delete expired sessions', () => {
+        jest.spyOn(Date, 'now').mockReturnValue(1000);
+        const keys1000 = [];
+        for (let i = 0; i < 3; i++) {
+            keys1000.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(2000);
+        const keys2000 = [];
+        for (let i = 0; i < 3; i++) {
+            keys2000.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(2500);
+        sessionStorage.deleteExpired(1000);
+
+        for (const key of keys1000) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
+        for (const key of keys2000) {
+            expect(sessionStorage.getSession(key)).toBe('data');
+        }
+    });
+
+    test('.startExpirationInterval should automatically expire items', () => {
+        jest.useFakeTimers();
+        const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+
+        sessionStorage.startExpirationInterval(ONE_DAY_MS * 7);
+
+        const keys = [];
+        for (let i = 0; i < 3; i++) {
+            keys.push(sessionStorage.createSession('data'));
+        }
+
+        const now = Date.now();
+        jest.spyOn(Date, 'now').mockReturnValue(now + ONE_DAY_MS * 3);
+        const keysThreeDaysLater = [];
+        for (let i = 0; i < 3; i++) {
+            keysThreeDaysLater.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(now + ONE_DAY_MS * 8);
+        jest.advanceTimersByTime(ONE_DAY_MS * 8);
+
+        for (const key of keys) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
+        for (const key of keysThreeDaysLater) {
+            expect(sessionStorage.getSession(key)).toBe('data');
+        }
+
+        Date.now.mockReturnValue(now + ONE_DAY_MS * 16);
+        jest.advanceTimersByTime(ONE_DAY_MS * 16);
+
+        for (const key of keysThreeDaysLater) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
     });
 });
 
