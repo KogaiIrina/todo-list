da5e9aa5bde9aa4357a66720e90394958ddc1ba1
Irina <63745301+KogayIrina@users.noreply.github.com>
2021-02-05 23:07:20 +0300

    Added function to deleting session (#8)

--- server/SessionStorage.js
+++ server/SessionStorage.js
@@ -1,22 +1,56 @@
 import crypto from 'crypto';
 
+const KEY_LENGTH = 256;
+const RELATIVE_EXPIRATION_FREQUENCY = 1 / 14;
+
+
 export default class SessionStorage {
     constructor() {
-        this.storage = {};
+        this._storage = {};
+    }
+
+    startExpirationInterval(shelfLifeMs) {
+        if (this._expirationInterval) {
+            this.stopExpirationInterval();
+        }
+        this._expirationInterval = setInterval(
+            () => this.deleteExpired(shelfLifeMs),
+            shelfLifeMs * RELATIVE_EXPIRATION_FREQUENCY
+        );
+    }
+
+    stopExpirationInterval() {
+        clearInterval(this._expirationInterval);
+        this._expirationInterval = undefined;
     }
 
     createSession(data) {
         let key;
         do {
-            key = crypto.randomBytes(256).toString('hex');
-        } while (key in this.storage);
+            key = crypto.randomBytes(KEY_LENGTH).toString('hex');
+        } while (key in this._storage);
 
-        this.storage[key] = data;
-
+        this._storage[key] = { data, accessedAt: Date.now() };
         return key;
     }
 
     getSession(key) {
-        return this.storage[key];
+        const session = this._storage[key];
+        if (session) {  
+          session.accessedAt = Date.now();
+          return session.data;
+        }
+    }
+
+    deleteExpired(shelfLifeMs) {
+        const bestBefore = Date.now() - shelfLifeMs;
+        for (const key in this._storage) {
+            if (!this._storage.hasOwnProperty(key)) {
+                continue;
+            }
+            if (this._storage[key].accessedAt < bestBefore) {
+                delete this._storage[key];
+            }
+        }
     }
 }
--- server/tests/SessionStorage.test.js
+++ server/tests/SessionStorage.test.js
@@ -1,8 +1,16 @@
+import { jest } from '@jest/globals';
+
 import SessionStorage from '../SessionStorage';
 
 describe('SessionStorage', () => {
     const sessionStorage = new SessionStorage();
     let key;
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+        jest.clearAllTimers();
+        sessionStorage.stopExpirationInterval();
+    });
 
     test('should return undefined on non-existent key', () => {
         expect(sessionStorage.getSession('non-existent key')).toBeUndefined();
@@ -25,6 +33,76 @@ describe('SessionStorage', () => {
         expect(oneMoreKey).not.toBe(key);
         expect(sessionStorage.getSession(oneMoreKey)).toBe('another data');
         expect(sessionStorage.getSession(key)).toBe('important data');
+    });
+
+    test('should create accessedAt on items in the storage', () => {
+        expect(typeof sessionStorage._storage[key].accessedAt).toBe('number');
+    });
+    
+    test('should update accessedAt on access', () => {
+        jest.spyOn(Date, 'now').mockReturnValue(111222333);
+        sessionStorage.getSession(key);
+        expect(sessionStorage._storage[key].accessedAt).toBe(111222333);
+    });
+
+    test('.deleteExpired should delete expired sessions', () => {
+        jest.spyOn(Date, 'now').mockReturnValue(1000);
+        const keys1000 = [];
+        for (let i = 0; i < 3; i++) {
+            keys1000.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(2000);
+        const keys2000 = [];
+        for (let i = 0; i < 3; i++) {
+            keys2000.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(2500);
+        sessionStorage.deleteExpired(1000);
+
+        for (const key of keys1000) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
+        for (const key of keys2000) {
+            expect(sessionStorage.getSession(key)).toBe('data');
+        }
+    });
+
+    test('.startExpirationInterval should automatically expire items', () => {
+        jest.useFakeTimers();
+        const ONE_DAY_MS = 1000 * 60 * 60 * 24;
+
+        sessionStorage.startExpirationInterval(ONE_DAY_MS * 7);
+
+        const keys = [];
+        for (let i = 0; i < 3; i++) {
+            keys.push(sessionStorage.createSession('data'));
+        }
+
+        const now = Date.now();
+        jest.spyOn(Date, 'now').mockReturnValue(now + ONE_DAY_MS * 3);
+        const keysThreeDaysLater = [];
+        for (let i = 0; i < 3; i++) {
+            keysThreeDaysLater.push(sessionStorage.createSession('data'));
+        }
+
+        Date.now.mockReturnValue(now + ONE_DAY_MS * 8);
+        jest.advanceTimersByTime(ONE_DAY_MS * 8);
+
+        for (const key of keys) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
+        for (const key of keysThreeDaysLater) {
+            expect(sessionStorage.getSession(key)).toBe('data');
+        }
+
+        Date.now.mockReturnValue(now + ONE_DAY_MS * 16);
+        jest.advanceTimersByTime(ONE_DAY_MS * 16);
+
+        for (const key of keysThreeDaysLater) {
+            expect(sessionStorage.getSession(key)).toBeUndefined();
+        }
     });
 });
 
