7821e50ed104e9582e9b96ae89678ffbb35642c6
Irina <63745301+KogayIrina@users.noreply.github.com>
2021-02-15 11:17:02 +0300

    Added post method to login

--- server/server.js
+++ server/server.js
@@ -5,9 +5,14 @@ import bson from 'bson';
 import bcrypt from 'bcrypt';
 
 import { Todo, User } from './mongodb';
+import SessionStorage from './SessionStorage';
 
 const ObjectId = bson.ObjectID;
 const SALT = '$2a$10$7h/0RT4RG5eX3602o3/.aO.RYkxKuhGkzvIXHLUiMJlFt1P.6Pe';
+const ONE_WEEK_MS = 1000 * 60* 60 * 24 * 7;
+const SESSION_STORAGE = new SessionStorage();
+
+SESSION_STORAGE.startExpirationInterval(ONE_WEEK_MS);
 
 const app = new Koa();
 const router = new Router();
@@ -46,6 +51,18 @@ router.post('/register', async ctx => {
     password: await bcrypt.hash(ctx.request.body.password, SALT)
   });
   await newUser.save();
+});
+
+router.post('/login', async ctx => {
+  const user = await User.findOne({ nickname: ctx.request.body.nickname });
+  ctx.assert(user, 401);
+
+  const match = await bcrypt.compare(ctx.request.body.password, user.password);
+  ctx.assert(match, 401);
+  
+  const session = SESSION_STORAGE.createSession(user._id);
+  ctx.cookies.set('session', session);
+  ctx.status = 200;
 });
 
 app
